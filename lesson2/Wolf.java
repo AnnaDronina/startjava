b709f0202d0e903c7d3e954f0f1cb6f1b87a66ae
AnnaDronina <martirocova-ann@yandex.ru>
2023-02-02 20:37:23 +0300

    Урок 2 ДЗ 2

---
+++ lesson2/Wolf.java
@@ -0,0 +1,29 @@
+public class Wolf {
+
+    String nickname;
+    int age;
+    String gender;
+    float weight;
+    String color;
+
+    void go() {
+        System.out.println("Идти");
+    }
+
+    void sit() {
+        System.out.println("Сидеть");
+    }
+
+    void run() {
+        System.out.println("Бежать");
+    }
+
+    void howl() {
+        System.out.println("Выть");
+    }
+
+    void hunt() {
+        System.out.println("Охотиться");
+    }
+
+}
