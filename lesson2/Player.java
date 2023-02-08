9e4c5a759f097ede22529d3d0322fe305a706fa5
AnnaDronina <martirocova-ann@yandex.ru>
2023-02-08 19:00:09 +0300

    Итоговое ДЗ Урок 2 задача 3

--- lesson2/Player.java
+++ lesson2/Player.java
@@ -1,38 +1,21 @@
-import java.util.Scanner;
-
 public class Player {
 
     private String name;
     private int number;
-    private boolean winner;
 
     public Player(String name) {
         this.name = name;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public void setWinner(boolean winner) {
-        this.winner = winner;
-    }
-
-    public boolean getWinner() {
-        return winner;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getNumber() {
-        return number;
+    public void setNumber(int number) {
+        this.number = number;
     }
 
-    public void guess() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(name + " угадайте число :"); 
-        number = scanner.nextInt();
+    public int getNumber() {
+        return number;
     }
 }
