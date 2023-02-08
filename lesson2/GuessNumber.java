a370ebdb90a0f8c724e57421b0de4a4414f21c82
AnnaDronina <martirocova-ann@yandex.ru>
2023-02-08 19:00:17 +0300

    Итоговое ДЗ Урок 2 задача 3

--- lesson2/GuessNumber.java
+++ lesson2/GuessNumber.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessNumber {
 
     private int hiddenNumber;
@@ -9,43 +12,41 @@ public class GuessNumber {
         this.player2 = player2;
     }
 
-    public int getHiddenNumber() {
-        return hiddenNumber;
-    }
-
-    public void setHiddenNumber(int hiddenNumber) {
-        this.hiddenNumber = hiddenNumber;
-    }
-
-    private Player playerChange(Player currentPlayer) {
-        if (currentPlayer == player1) {
-            return player2;
-        }
-        return player1;
-    }
-
-    public void startPlay() {
+    public void play() {
         Player currentPlayer = player1;
-        while (!currentPlayer.getWinner()) {
-            currentPlayer.guess();
+        Random random = new Random();
+        hiddenNumber = random.nextInt(100)+1;
+        System.out.println(hiddenNumber);// убрать
+        boolean winner = false;
+
+        while (!winner) {
+            guess(currentPlayer);
             if (currentPlayer.getNumber() > 0 && currentPlayer.getNumber() <= 100) {
                 if (currentPlayer.getNumber() > hiddenNumber) {
                     System.out.println("Число " + currentPlayer.getNumber() + 
                             " больше того, что загадал компьютер");
-                    currentPlayer = playerChange(currentPlayer);
                 } else if (currentPlayer.getNumber() < hiddenNumber) {
                     System.out.println("Число " + currentPlayer.getNumber() + 
                             " меньше того, что загадал компьютер");
-                    currentPlayer = playerChange(currentPlayer);
                 } else {
                     System.out.println(currentPlayer.getName() + " Вы победили");
-                    currentPlayer.setWinner(true);
+                    winner = true;
                 }
             } else {
                 System.out.println("Введенное число не находится в полуинтервале (0, 100]");
-                currentPlayer = playerChange(currentPlayer);
             }
+            currentPlayer = playerChange(currentPlayer);
         }
-        currentPlayer.setWinner(false);
     }
+
+    private void guess(Player currentPlayer) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(currentPlayer.getName() + " угадайте число :"); 
+        currentPlayer.setNumber(scanner.nextInt());
+    }
+
+    private Player playerChange(Player currentPlayer) {
+        return currentPlayer == player1 ? player2 : player1;
+    }
+
 }
