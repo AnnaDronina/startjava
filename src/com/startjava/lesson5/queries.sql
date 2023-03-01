46e5a20e963b0a32d00f078183e9505d3dc4ea2d
AnnaDronina <martirocova-ann@yandex.ru>
2023-03-01 12:32:47 +0300

    ДЗ 5

--- src/com/startjava/lesson5/queries.sql
+++ src/com/startjava/lesson5/queries.sql
@@ -1,53 +1,51 @@
--- Вывод содержимого таблицы Jaegers
+\C 'Вывод содержимого таблицы Jaegers'
 SELECT *
   FROM jaegers;
 
--- Вывод не уничтоженных роботов
+\C 'Вывод не уничтоженных роботов'
 SELECT *
   FROM jaegers
  WHERE status = 'Active';
 
--- Вывод роботов нескольких серий: Mark-1 и Mark-4
+\C 'Вывод роботов нескольких серий: Mark-1 и Mark-4'
 SELECT *
   FROM jaegers
  WHERE mark IN ('Mark-1', 'Mark-4');
 
--- Вывод всех роботов, кроме Mark-1 и Mark-4
+\C 'Вывод всех роботов, кроме Mark-1 и Mark-4'
 SELECT *
   FROM jaegers
  WHERE mark NOT IN ('Mark-1', 'Mark-4');
 
--- Сортировка таблицы по убыванию по столбцу mark
+\C 'Сортировка таблицы по убыванию по столбцу mark'
   SELECT *
     FROM jaegers
 ORDER BY mark DESC;
 
--- Вывод информации о самом старом роботе
+\C 'Вывод информации о самом старом роботе'
 SELECT *
   FROM jaegers
  WHERE launch =
        (SELECT MIN(launch)
           FROM jaegers);
 
--- Вывод роботов, которые уничтожили больше всех kaiju
+\C 'Вывод роботов, которые уничтожили больше всех kaiju'
 SELECT *
   FROM jaegers
  WHERE kaijukill =
        (SELECT MAX(kaijukill)
        FROM jaegers);
 
-
--- Вывод среднего веса роботов
+\C 'Вывод среднего веса роботов'
 SELECT AVG(weight)
   FROM jaegers;
 
-
--- Увеличение на единицу количество уничтоженных kaiju у роботов, которые до сих пор не разрушены
+\C 'Увеличение на единицу количество уничтоженных kaiju у роботов, которые до сих пор не разрушены'
 UPDATE jaegers
    SET kaijukill = kaijukill + 1
  WHERE status = 'Active';
 
--- Удаление уничтоженных роботов
+\C 'Удаление уничтоженных роботов'
 DELETE
   FROM jaegers
  WHERE status = 'Destroyed';
